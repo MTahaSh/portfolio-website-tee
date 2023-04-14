1677bb1e19b8308146767818795f143031a6de78
MTahaSh <tahashayk2k@gmail.com>
2023-04-15 02:18:54 +0500

    wrapping

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -13,42 +13,42 @@ export default function Gallery() {
     
     <div className='border pr-2 pb-64 pl-2 '>
         <h1 className='font-bold text-xl'>Panaverse DAO Website</h1>
-        <hr />
+        {<hr />}
         <p className='leading-7'>The panaverse dao website's front-end is developed using next js and tailwind CSS. <Link className='text-blue-400' target='_blank' href={"https://github.com/MTahaSh/panaverse-dao-tailwind-tee"}>Learn More</Link></p>
         
     </div>
     
 <div className='border pr-2 pb-64 pl-2 '>
         <h1 className='font-bold text-xl'>Currency Exchange using Typescript</h1>
-        <hr />
+        {<hr />}
         <p className='leading-7'>This project has been developed using typescript. A typescript is a framework used to provide typesafety for the values in javascript. Moreover, node package manager is used for uploading and downloading of the project. </p>
         <Link className='text-blue-400' target='_blank' href={"https://github.com/MTahaSh/currency-exchange-using-ts"}>Learn More</Link>
     </div>
     
 <div className='border pr-2 pb-64 pl-2 '>
         <h1 className='font-bold text-xl'>To-Do List using Typescript</h1>
-        <hr />
+        {<hr />}
         <p className='leading-7'>This project is developed using typescript and there is a unique logic used for developing a to-do list. </p>
         <Link className='text-blue-400' target='_blank' href={"https://github.com/MTahaSh/to-do-list-using-ts"}>Learn More</Link>
     </div>
     
 <div className='border pr-2 pb-64 pl-2 '>
         <h1 className='font-bold text-xl'>Guessing Game using Typescript</h1>
-        <hr />
+        {<hr />}
         <p>The following game is developed using typescript with few dependencies such as inquirer, chalk and chalk-animation.  </p>
         <Link className='text-blue-400' target='_blank' href={"https://github.com/MTahaSh/Guessing-Game-using-Typescript"}>Learn More</Link>
     </div>
     
 <div className='border pr-2 pb-64 pl-2 '>
         <h1 className='font-bold text-xl'>Price tag UI</h1>
-        <hr />
+        {<hr />}
         <p>This following price tag ui is created using chakra Ui framework and next js. </p>
         <Link className='text-blue-400' target='_blank' href={"https://github.com/MTahaSh/price-tag-ui-next-tee"}>Learn More</Link> 
     </div>
     
 <div className='border pr-2 pb-64 pl-2 '>
         <h1 className='font-bold text-xl'>Spotify Clone</h1>
-        <hr />
+        {<hr />}
         <p>This following spotify clone is not completely but almost responsive. It was developed using html, css and javascript vanilla. </p>
         <Link className='text-blue-400' target='_blank' href={"https://github.com/MTahaSh/Spotify-Clone-Almost-Responsive-"}>Learn More</Link>
     </div>
@@ -56,7 +56,7 @@ export default function Gallery() {
 </div>
 
     </div>
-    
+
     )
     
 }
